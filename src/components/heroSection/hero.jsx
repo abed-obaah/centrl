63d9313d3af88d82391374e81c88d255fda00338
abed obaah <ubahobaah@gmail.com>
2025-01-08 18:17:26 +0100

    home

--- src/components/heroSection/hero.jsx
+++ src/components/heroSection/hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Stars from '../../assets/stars.png';
 import Future from '../../assets/future.png';
+import Share from '../../assets/share.png';
 
 function Hero() {
     return (
@@ -18,10 +19,21 @@ function Hero() {
                                 src={Future}
                                 className="h-80 w-auto object-contain"
                             />
+                           <div className="flex items-center space-x-4 mt-4">
+                                    <img
+                                        alt=""
+                                        src={Share}
+                                        className="h-4 w-auto object-contain" // Reduced size of the image
+                                    />
 
-                    <p className="text-5xl font-bold text-gray-900">Eventd worth yout time</p>
+                                    <p className="text-[18px] font-normal text-transparent bg-clip-text bg-gradient-to-r from-[#3EA969] via-[#9374D5] to-[#FBB123]">
+                                        Events worth <br /> your time
+                                    </p>
+                         </div>
+
+                        
+
                 </div>
-              
                
             </div>
         </div>
