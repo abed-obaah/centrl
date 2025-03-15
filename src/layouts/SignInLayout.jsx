611ce67bf3d7ecb7d1e3a22b290322e39c00bffa
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2025-03-15 22:08:25 +0100

    Chore:made some changes to the sign in page

--- src/layouts/SignInLayout.jsx
+++ src/layouts/SignInLayout.jsx
@@ -1,38 +1,39 @@
-import { Link } from 'react-router-dom';
-import logo from '../assets/logo.png';
-import WaitlistImg from '../assets/waitlist-image.png';
-import SignInForm from '../components/SigninForm';
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import SignImg from "../assets/sign-in-image.png";
+import SignInForm from "../components/SigninForm";
 
 const SignInLayout = () => {
   return (
     <div className="min-h-screen">
       <main className="h-screen">
-        <div className="max-w-[1290px] 2xl:max-w-[1500px] mx-auto relative">
-          <div className="absolute z-10 top-2 left-0 mt-4 ml-4">
-            <Link to="/" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+        <div className="relative mx-auto max-w-[1290px] 2xl:max-w-[1500px]">
+          <div className="absolute left-0 top-2 z-10 ml-4 mt-4">
+            <Link
+              to="/"
+              onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            >
               <img alt="Centrl" src={logo} className="size-7" loading="lazy" />
             </Link>
           </div>
         </div>
 
-        <div className="xl:grid xl:grid-cols-2 h-full">
+        <div className="h-full xl:grid xl:grid-cols-2">
           {/* img */}
-          <div className="container flex items-center justify-center h-full xl:fixed xl:w-1/2  xl:overflow-hidden">
+          <div className="container flex h-full items-center justify-center xl:fixed xl:w-1/2 xl:overflow-hidden">
             <img
-              className="hidden xl:block xl:size-[700px] xl:mt-[-1rem]  xl:mx-auto 2xl:my-auto  xl:object-cover"
-              src={WaitlistImg}
-              alt="Waitlist"
+              className="hidden xl:mx-auto xl:mt-[-1rem] xl:block xl:size-[500px] xl:object-cover 2xl:my-auto"
+              src={SignImg}
+              alt="Sign In Image"
               loading="lazy"
             />
           </div>
           {/* form */}
-          {/* form */}
-            <div className="xl:bg-white py-16 xl:py-0 xl:col-start-2 xl:min-h-screen xl:overflow-y-auto">
-            <div className="container flex items-center justify-center min-h-screen">
-                <SignInForm />
+          <div className="py-16 xl:col-start-2 xl:min-h-screen xl:overflow-y-auto xl:bg-white xl:py-0">
+            <div className="container flex min-h-screen items-center justify-center">
+              <SignInForm />
             </div>
-            </div>
-
+          </div>
         </div>
       </main>
     </div>
