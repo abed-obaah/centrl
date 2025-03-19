82568a5dbf3ca97b850bf9f2573d806e6ef9fa26
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2025-03-19 14:09:26 +0100

    chore:added the logic for the signup page

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: "/",
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://api.centrl.ng",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  // server: {
+  //   port: 3000,
+
+  //   "/api.centrl.ng": {
+  //     target: "https://api.centrl.ng",
+  //     changeOrigin: true,
+  //     rewrite: (path) => path.replace(/^\/api/, ""),
+  //   },
+  // },
 });
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server:{
-//     port: 3000
-//   }
-// })
